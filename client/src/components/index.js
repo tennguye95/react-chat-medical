6d9945153dd2fd7080ba6fb3581929c368968b02
sacred95 <sacred9529@gmail.com>
2021-09-17 15:28:35 -0700

    initialized

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+export { default as ChannelContainer } from './ChannelContainer.jsx';
+export { default as ChannelListContainer } from './ChannelListContainer.jsx';
+export { default as ChannelSearch } from './ChannelSearch.jsx';
+export { default as TeamChannelList } from './TeamChannelList.jsx';
+export { default as TeamChannelPreview } from './TeamChannelPreview.jsx';
+export { default as Auth } from './Auth.jsx';
+export { default as ChannelInner} from './ChannelInner.jsx';
+export { default as CreateChannel } from './CreateChannel.jsx';
+export { default as EditChannel } from './EditChannel.jsx';
+export { default as TeamMessage } from './TeamMessage.jsx';
+export { default as UserList } from './UserList.jsx';
+export { default as ResultsDropdown } from './ResultsDropdown.jsx';
